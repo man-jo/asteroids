e699617070f63f8176b5544a478316813da45488
Jonas Mandel <jonas.mandel@gmail.com>
2025-04-08 11:09:16 +0200

    update: add shots and asteroids, and start shooting logic

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame
+import sys
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
 
@@ -11,17 +15,38 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+
+
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
 
     while True:
-        screen.fill(color="black")
-        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60)
+        screen.fill(color="black")
+        updatable.update(dt)
+        for ast in asteroids:
+            for s in shots:
+                if ast.get_collision(s):
+                    ast.kill()
+                    s.kill()
+            if ast.get_collision(player):
+                print("Game Over !")
+                sys.exit()
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -1,12 +1,14 @@
 import pygame
 from constants import *
-from circleshape import *
+from circleshape import CircleShape
+from shot import Shot
 
-# Base class for game objects
 class Player(CircleShape):
+
     def __init__(self, x, y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     # in the player class
     def triangle(self):
@@ -19,4 +21,31 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, color="white", points=self.triangle(), width=2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_q]:
+            self.rotate(-dt)
+        if keys[pygame.K_z]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+        self.timer -= dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        if self.timer <= 0:
+            self.timer = PLAYER_SHOOT_COOLDOWN
+            shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
